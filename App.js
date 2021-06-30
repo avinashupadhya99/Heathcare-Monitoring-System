b53aba4dd8d653a518063916e33279065c51c637
Avinash Upadhyaya <avinashupadhya99@gmail.com>
2021-06-30 17:44:25 +0530

    feat: display charts

--- App.js
+++ App.js
@@ -1,112 +1,99 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, ScrollView, Text} from 'react-native';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import Header from './components/Header';
+import Chart from './components/Chart';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const [feedValues, setFeedValues] = useState({});
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+  useEffect(() => {
+    fetch(
+      'https://api.thingspeak.com/channels/1268340/feeds.json?api_key=RCW348S17GG6FXWT',
+    )
+      .then(feedData => feedData.json())
+      .then(feedData => {
+        // console.log(feedData);
+        let feedValue = {};
+        feedData.feeds.map(feed => {
+          // Heart Beat Sensor
+          if (feedValue[feedData.channel.field1] === undefined) {
+            feedValue[feedData.channel.field1] = {
+              datetime: [],
+              value: [],
+            };
+          }
+          feedValue[feedData.channel.field1].datetime.push(
+            new Date(feed.created_at).toTimeString().substring(0, 5), // Get time in hh:mm
+          );
+          feedValue[feedData.channel.field1].value.push(feed.field1);
+          // Body Temperature Sensor (LM35)
+          if (feedValue[feedData.channel.field2] === undefined) {
+            feedValue[feedData.channel.field2] = {
+              datetime: [],
+              value: [],
+            };
+          }
+          feedValue[feedData.channel.field2].datetime.push(
+            new Date(feed.created_at).toTimeString().substring(0, 5), // Get time in hh:mm
+          );
+          feedValue[feedData.channel.field2].value.push(feed.field2);
+          // Room Temperature Sensor (DHT11)
+          if (feedValue[feedData.channel.field3] === undefined) {
+            feedValue[feedData.channel.field3] = {
+              datetime: [],
+              value: [],
+            };
+          }
+          feedValue[feedData.channel.field3].datetime.push(
+            new Date(feed.created_at).toTimeString().substring(0, 5), // Get time in hh:mm
+          );
+          feedValue[feedData.channel.field3].value.push(feed.field3);
+          // CO Sensor (MQ-9)
+          if (feedValue[feedData.channel.field4] === undefined) {
+            feedValue[feedData.channel.field4] = {
+              datetime: [],
+              value: [],
+            };
+          }
+          feedValue[feedData.channel.field4].datetime.push(
+            new Date(feed.created_at).toTimeString().substring(0, 5), // Get time in hh:mm
+          );
+          feedValue[feedData.channel.field4].value.push(feed.field4);
+          // CO2 Sensor (MQ-135)
+          if (feedValue[feedData.channel.field5] === undefined) {
+            feedValue[feedData.channel.field5] = {
+              datetime: [],
+              value: [],
+            };
+          }
+          feedValue[feedData.channel.field5].datetime.push(
+            new Date(feed.created_at).toTimeString().substring(0, 5), // Get time in hh:mm
+          );
+          feedValue[feedData.channel.field5].value.push(feed.field5);
+        });
+        // console.log(feedValue);
+        setFeedValues(feedValue);
+      })
+      .catch(fetchError => {
+        console.error(fetchError);
+      });
+  }, []);
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
+    <SafeAreaView>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
         <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+        {Object.keys(feedValues).map((feedValue, index) => (
+          <Chart
+            name={feedValue}
+            feedValues={feedValues[feedValue]}
+            key={index}
+          />
+        ))}
       </ScrollView>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
---
+++ components/Chart.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {Dimensions, StyleSheet, Text, View, ScrollView} from 'react-native';
+import {LineChart} from 'react-native-chart-kit';
+
+const Chart = ({name, feedValues}) => {
+  return (
+    <View>
+      <Text>{name}</Text>
+      <ScrollView
+        horizontal={true}
+        contentOffset={{x: 10000, y: 0}} // i needed the scrolling to start from the end not the start
+        showsHorizontalScrollIndicator={false} // to hide scroll bar
+      >
+        <LineChart
+          data={{
+            labels: feedValues.datetime,
+            datasets: [
+              {
+                data: feedValues.value,
+              },
+            ],
+            legend: [name],
+          }}
+          width={feedValues.value.length * 50} // from react-native
+          height={220}
+          yAxisLabel=""
+          yAxisSuffix=""
+          yAxisInterval={20} // optional, defaults to 1
+          chartConfig={{
+            backgroundColor: '#e26a00',
+            backgroundGradientFrom: '#fb8c00',
+            backgroundGradientTo: '#ffa726',
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16,
+            },
+            propsForDots: {
+              r: '6',
+              strokeWidth: '2',
+              stroke: '#ffa726',
+            },
+          }}
+          bezier
+          style={styles.lineChart}
+        />
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  lineChart: {
+    marginVertical: 8,
+    borderRadius: 16,
+  },
+});
+
+export default Chart;

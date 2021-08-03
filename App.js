8de8a354747de7ab5ca1632eb282d1f4892d8acc
Avinash Upadhyaya <avinashupadhya99@gmail.com>
2021-08-03 22:11:01 +0530

    feat: auto refresh 30 seconds

--- App.js
+++ App.js
@@ -8,13 +8,14 @@ import {
   TextInput,
   View,
 } from 'react-native';
+import BackgroundTimer from 'react-native-background-timer';
 import {CourierClient} from '@trycourier/courier';
 
 import Header from './components/Header';
 import Chart from './components/Chart';
 
 // To force update the text input (update threshold input)
-function useForceUpdate(){
+function useForceUpdate() {
   const [value, setValue] = useState(0); // integer state
   return () => setValue(value => value + 1); // update the state to force render
 }
@@ -29,6 +30,103 @@ const App = () => {
   const [selectedFeedNumber, setSelectedFeedNumber] = useState(0);
   const [availableFeeds, setAvailableFeeds] = useState([]);
   const [textRef, setTextRef] = useState(null);
+
+  BackgroundTimer.runBackgroundTimer(() => {
+    fetch(
+      'https://api.thingspeak.com/channels/1268340/feeds.json?api_key=RCW348S17GG6FXWT&days=1&round=2',
+    )
+      .then(feedData => feedData.json())
+      .then(feedData => {
+        // console.log(feedData);
+        let feedValue = {};
+        feedData.feeds.map(feed => {
+          // Heart Beat Sensor
+          if (feedValue[feedData.channel.field1] === undefined) {
+            // Initialize
+            feedValue[feedData.channel.field1] = {
+              datetime: [],
+              value: [],
+            };
+          }
+          feedValue[feedData.channel.field1].datetime.push(
+            new Date(feed.created_at).toTimeString().substring(0, 5), // Get time in hh:mm
+          );
+          feedValue[feedData.channel.field1].value.push(feed.field1);
+          if (feed.field1 > thresholds.field1) {
+            sendNotification({
+              feed: feedData.channel.field1,
+              threshold: thresholds.field1,
+              feedValue: feed.field1,
+              dateTime: new Date(feed.created_at).toString(),
+            });
+          }
+          // Body Temperature Sensor (LM35)
+          if (feedValue[feedData.channel.field2] === undefined) {
+            // Initialize
+            feedValue[feedData.channel.field2] = {
+              datetime: [],
+              value: [],
+            };
+          }
+          feedValue[feedData.channel.field2].datetime.push(
+            new Date(feed.created_at).toTimeString().substring(0, 5), // Get time in hh:mm
+          );
+          feedValue[feedData.channel.field2].value.push(feed.field2);
+          if (feed.field2 > thresholds.field2) {
+            sendNotification({
+              feed: feedData.channel.field2,
+              threshold: thresholds.field2,
+              feedValue: feed.field2,
+              dateTime: new Date(feed.created_at).toString(),
+            });
+          }
+          // Room Temperature Sensor (DHT11)
+          if (feedValue[feedData.channel.field3] === undefined) {
+            // Initialize
+            feedValue[feedData.channel.field3] = {
+              datetime: [],
+              value: [],
+            };
+          }
+          feedValue[feedData.channel.field3].datetime.push(
+            new Date(feed.created_at).toTimeString().substring(0, 5), // Get time in hh:mm
+          );
+          feedValue[feedData.channel.field3].value.push(feed.field3);
+          // CO Sensor (MQ-9)
+          if (feedValue[feedData.channel.field4] === undefined) {
+            // Initialize
+            feedValue[feedData.channel.field4] = {
+              datetime: [],
+              value: [],
+            };
+          }
+          feedValue[feedData.channel.field4].datetime.push(
+            new Date(feed.created_at).toTimeString().substring(0, 5), // Get time in hh:mm
+          );
+          feedValue[feedData.channel.field4].value.push(feed.field4);
+          // CO2 Sensor (MQ-135)
+          if (feedValue[feedData.channel.field5] === undefined) {
+            // Initialize
+            feedValue[feedData.channel.field5] = {
+              datetime: [],
+              value: [],
+            };
+          }
+          feedValue[feedData.channel.field5].datetime.push(
+            new Date(feed.created_at).toTimeString().substring(0, 5), // Get time in hh:mm
+          );
+          feedValue[feedData.channel.field5].value.push(feed.field5);
+        });
+        // console.log(feedValue);
+        setFeedValues(feedValue);
+        // Set the first field as the default field
+        setAvailableFeeds(Object.keys(feedValue));
+        // console.log(feedValues);
+      })
+      .catch(fetchError => {
+        console.error(fetchError);
+      });
+  }, 30000);
 
   const sendNotification = async data => {
     const {messageId} = await courier.send({
